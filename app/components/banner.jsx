d2a31c8ae17a55745282293c7a290fc20596c891
Gabriel Bach <gabriel@inter-action.design>
2023-10-18 16:39:13 +0200

    add animation to banner

--- app/components/banner.jsx
+++ app/components/banner.jsx
@@ -1,8 +1,10 @@
-// 'use client'
+'use client'
 
-// import { useEffect, useState, useRef } from "react"
+import { useEffect, useState, useRef } from "react"
 
 const BannerComponent = ({ globalSet: { email, date, time} }) => {
+
+    const bannerRef = useRef(null)
 
     const handleScroll = (e) => {
 
@@ -14,22 +16,21 @@ const BannerComponent = ({ globalSet: { email, date, time} }) => {
         bannerRef.current.scrollTo((bannerWidth - window.innerWidth) * scrollPercent, 0);
     }
 
-    // useEffect(() => {
-    //     window.addEventListener('scroll', handleScroll)
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll)
 
-    //     return () => {
-    //         window.removeEventListener('scroll', handleScroll)
-    //     }
-    // }, [])
+        return () => {
+            window.removeEventListener('scroll', handleScroll)
+        }
+    }, [])
 
     return (
         <nav className="border-t-2 border-black fixed z-20 bottom-0 left-0 w-full bg-white text-sm md:text-lg">
-            <div className="min-h-[100px] h-20 overflow-scroll whitespace-nowrap z-10 pointer-events-all">
+            <div ref={bannerRef} className="min-h-[100px] h-20 overflow-scroll whitespace-nowrap z-10 pointer-events-all">
                 <BannerInner date={date} time={time}/>
-                {/* <BannerInner content={content} />
-                <BannerInner content={content} /> */}
+                <BannerInner date={date} time={time}/>
             </div>
-            <div className="w-full flex justify-between lg:hidden">
+            <div className="w-full flex justify-between lg:hidden px-2 md:px-4">
                 <div>{date}</div>
                 <div>{time}</div>
             </div>
@@ -39,7 +40,7 @@ const BannerComponent = ({ globalSet: { email, date, time} }) => {
 
 const BannerInner = ({ date, time }) => {
     return(
-        <div className="inline-flex w-screen 2xl:w-[1800px] gap-4 h-full justify-between align-center mr-2 md:mr-4 px-2 md:px-4">
+        <div className="inline-flex w-screen 2xl:w-[1800px] gap-4 h-full justify-between align-center mr-2 last:mr-0 md:mr-4 px-2 md:px-4">
             <div className={`h-full flex items-center justify-center order-10`}>
                 <img className={`h-[50%]`} src={`/images/schlange-outline.svg`} alt="" />
             </div>
