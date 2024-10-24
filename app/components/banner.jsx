c2913b8acba33d954074366b9dc5b52edb18d04c
Gabriel Bach <gabrielmolembi@hotmail.com>
2024-10-24 09:58:38 +0200

    fix banner

--- app/components/banner.jsx
+++ app/components/banner.jsx
@@ -40,27 +40,43 @@ const BannerComponent = ({ globalSet: { email, date, time} }) => {
 
 const BannerInner = ({ date, time }) => {
     return(
-        <div className="inline-flex w-screen 2xl:w-[1800px] gap-4 h-full justify-between align-center mr-2 last:mr-0 lg:mr-4 px-2 lg:px-4">
-            <div className={`h-full items-center justify-center  hidden lg:flex`}>
-                <img className={`h-[50%]`} src={`/images/schlange-outline.svg`} alt="" />
-            </div>
+        <div className="inline-flex w-screen gap-4 h-full justify-between align-center mr-2 last:mr-0 lg:mr-4 px-2 lg:px-4">
             <div className={`h-full items-center items-center justify-center hidden lg:flex `}>
                 <div>{date}</div>
             </div>
-            {/* <div className={`h-full flex items-center justify-center order-3 lg:order-none basis-[30%] lg:basis-auto`}>
-                <img className={`h-[100%]`} src={`/images/fuenf-jahres-stern.svg`} alt="" />
-            </div> */}
-            <div className={`h-full flex items-center justify-center order-2 lg:order-none basis-[40%] lg:basis-auto`}>
+            <div className="flex justify-center lg:justify-between flex-grow-[1] gap-4">
+                <div className={`h-full items-center justify-center  hidden lg:flex`}>
+                    <img className={`h-[50%]`} src={`/images/vermi-banner-wurm-desktop-outline.svg`} alt="" />
+                </div>
+                <div className={`h-full items-center justify-center  lg:hidden flex`}>
+                    <img className={`h-[50%]`} src={`/images/vermi-banner-wurm-mobile-outline.svg`} alt="" />
+                </div>
+                <div className={`h-full items-center justify-center  hidden lg:flex`}>
+                    <img className={`h-[50%]`} src={`/images/vermi-banner-meringues-outline.svg`} alt="" />
+                </div>
+                <div className={`h-full items-center justify-center  hidden lg:flex`}>
+                    <img className={`h-[50%] translate-y-[5%]`} src={`/images/vermicelleria-banner-halbkreis-outline.svg`} alt="" />
+                </div>
+            </div>
+            <div className={`h-full flex mx-4 items-center justify-center basis-[40%] lg:basis-auto`}>
                 <img className={`h-[80%]`} src={`/images/logo-vermicelleria-schwarz.svg`} alt="" />
             </div>
-            {/* <div className={`h-full flex items-center justify-center order-1 lg:order-none basis-[30%] lg:basis-auto`}>
-                <img className={`h-[80%]`} src={`/images/jubilaeums-banner.svg`} alt="" />
-            </div> */}
+            <div className="flex justify-center lg:justify-between flex-grow-[1] gap-4">
+                <div className={`h-full items-center justify-center flex`}>
+                    <img className={`h-[50%]`} src={`/images/vermi-banner-meringues-outline.svg`} alt="" />
+                </div>
+                <div className={`h-full items-center justify-center  hidden lg:flex`}>
+                    <img className={`h-[50%]`} src={`/images/vermi-banner-wurm-desktop-outline.svg`} alt="" />
+                </div>
+                <div className={`h-full items-center justify-center  hidden lg:flex`}>
+                    <img className={`h-[50%] translate-y-[5%]`} src={`/images/vermicelleria-banner-halbkreis-outline.svg`} alt="" />
+                </div>
+                <div className={`h-full items-center justify-center  hidden lg:flex`}>
+                    <img className={`h-[50%]`} src={`/images/vermi-banner-wurm-mobile-outline.svg`} alt="" />
+                </div>
+            </div>
             <div className={`h-full items-center items-center justify-center hidden lg:flex `}>
                 <div>{time}</div>
-            </div>
-            <div className={`h-full flex items-center justify-center  hidden lg:flex`}>
-                <img className={`h-[60%]`} src={`/images/2er-wolke-outline.svg`} alt="" />
             </div>
         </div>
     )
